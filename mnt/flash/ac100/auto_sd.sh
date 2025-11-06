2dc9be4c1be8db65a3b3180e86a81691a17857bf
Lance Lu <lance.cl.lu@gmail.com>
2025-11-06 09:27:22 +0800

    2510031308

--- mnt/flash/ac100/auto_sd.sh
+++ mnt/flash/ac100/auto_sd.sh
@@ -4,14 +4,22 @@ export LD_LIBRARY_PATH=/mnt/flash/vienna/lib:/mnt/flash/vienna/lib/vienna:$LD_LI
 
 VREC_PATH="/mnt/flash/ac100"
 SD_MOUNT_PATH="/mnt/sd"
+SD_MOUNT_PATH_TMP="/tmp/vrecord/videoclips"
 VREC_CONF="vrec_conf.ini"
+APP_NAME="kp_firmware_host_stream_app_babycam_audio"
+LOG_FILE="/tmp/sd_event.log"
 
 printf "\n\n\n===== auto_sd.sh =====\n\n"
 
+APP_PID=$(pidof "$APP_NAME")
+
 if [ "$ACTION" = "add" ] || [ -b /dev/mmcblk0p1 ]; then
-    mkdir -p /tmp/vrecord/videoclips/
-    mount /dev/mmcblk0p1 /tmp/vrecord/videoclips
+    echo "[auto_sd.sh] SD card inserted" >> "$LOG_FILE"
+
+    mkdir -p "$SD_MOUNT_PATH_TMP"
+    mount /dev/mmcblk0p1 "$SD_MOUNT_PATH_TMP"
     mount /dev/mmcblk0p1 "$SD_MOUNT_PATH"
+
     cd "$VREC_PATH"
 
     if ps | grep "[v]rec" > /dev/null; then
@@ -21,7 +29,25 @@ if [ "$ACTION" = "add" ] || [ -b /dev/mmcblk0p1 ]; then
         ./vrec -c "$VREC_CONF" &
     fi
 
-elif [ "$ACTION" = "remove" ]; then
+    if [ -n "$APP_PID" ]; then
+       kill -44 "$APP_PID"
+        echo "[auto_sd.sh] Sent SIGRTMIN (insert) to $APP_NAME (PID=$APP_PID)" >> "$LOG_FILE"
+    else
+        echo "[auto_sd.sh] $APP_NAME not running. Signal not sent." >> "$LOG_FILE"
+    fi
+
+#elif [ "$ACTION" = "remove" ]; then
+else
+    echo "[auto_sd.sh] SD card removed" >> "$LOG_FILE"
+
     killall vrec
     umount "$SD_MOUNT_PATH"
+    umount "$SD_MOUNT_PATH_TMP"
+
+    if [ -n "$APP_PID" ]; then
+        kill -45 "$APP_PID"
+        echo "[auto_sd.sh] Sent SIGRTMIN+1 (remove) to $APP_NAME (PID=$APP_PID)" >> "$LOG_FILE"
+    else
+        echo "[auto_sd.sh] $APP_NAME not running. Signal not sent." >> "$LOG_FILE"
+    fi
 fi
